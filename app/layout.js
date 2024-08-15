67bdc543f00e8cf7bf0dcf4cf51067dece011a54
Shrey Shivhare <khiladi2461997@gmail.com>
2024-08-15 12:22:52 +0530

    added frontend pages

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "../styles/main.scss";
-//import Providers from "@/context/Providers";
+import Providers from "@/context/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        {/*<Providers>{children}</Providers>*/}
+        <Providers>{children}</Providers>
         {children}
       </body>
     </html>
