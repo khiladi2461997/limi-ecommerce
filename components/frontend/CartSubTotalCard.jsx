21f046dac46eec639832738cdbd149127790fbbf
Shrey Shivhare <khiladi2461997@gmail.com>
2024-08-18 18:46:53 +0530

    updated INR4

--- components/frontend/CartSubTotalCard.jsx
+++ components/frontend/CartSubTotalCard.jsx
@@ -14,15 +14,15 @@ export default function CartSubTotalCard({ subTotal }) {
       <h2 className="text-2xl pb-3">Cart total</h2>
       <div className="flex items-center justify-between border-b border-slate-500 pb-6">
         <span>Subtotal </span>
-        <span>UGX{subTotal}</span>
+        <span>INR{subTotal}</span>
       </div>
       <div className="flex items-center justify-between pb-4 mt-2">
         <span>Tax </span>
-        <span>UGX{tax}</span>
+        <span>INR{tax}</span>
       </div>
       <div className="flex items-center justify-between pb-4">
         <span>Shipping </span>
-        <span>UGX{shipping}</span>
+        <span>INR{shipping}</span>
       </div>
       <p className="border-b border-slate-500 pb-6 text-slate-400 font-normal">
         We only charge for shipping when you have over 2kg items
